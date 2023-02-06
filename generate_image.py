f69764893448252abb9d42756ed4fa16f2b43e59
Zheng Yune <yunethecoder@gmail.com>
2023-02-06 16:18:04 -0500

    update generate_image.py

--- generate_image.py
+++ generate_image.py
@@ -1,18 +1,23 @@
+# Import necessary packages
 import os
 import openai
 import requests
 import io
 from PIL import Image
+# Set the API key for the OpenAI API
 openai.organization = "org-ib1ZWJAduDkTLlWWceXwAUes"
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
+# Use OpenAI API to create an image based on the given prompt
 response = openai.Image.create(
-    prompt="a brilliant and handsome talent computer science master student name Yune who",
+    prompt="a fat and talent Chinese male computer science   student named Jerry ",
     n=1,
     size="1024x1024"
 )
+# Get the URL of the generated image
 image_url = response['data'][0]['url']
+# Use requests to retrieve the image content from the URL
 response = requests.get(image_url)
+# Use the Image package from PIL to open the image content
 img = Image.open(io.BytesIO(response.content))
-# img = Image.open(image_url)
-img.save("img/yune.jpg")
+# Save the image to a file named "yune.jpg" in the "img" folder
+img.save("img/leren.jpg")
