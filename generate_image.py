7ada712b0d16f5ac7f6a3221fa03322924ce808e
Zheng Yune <yunethecoder@gmail.com>
2023-02-08 18:31:51 -0500

    update

--- generate_image.py
+++ generate_image.py
@@ -8,20 +8,22 @@ from PIL import Image
 openai.organization = "org-ib1ZWJAduDkTLlWWceXwAUes"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # Use OpenAI API to create an image based on the given prompt
-your_description = input("Enter your description for the image you want: ")
-# print(your_description)
-response = openai.Image.create(
-    prompt=your_description,
-    n=1,
-    size="1024x1024"
-)
-# Get the URL of the generated image
-image_url = response['data'][0]['url']
-# Use requests to retrieve the image content from the URL
-response = requests.get(image_url)
-# Use the Image package from PIL to open the image content
-img = Image.open(io.BytesIO(response.content))
-# Save the image to a file named "yune.jpg" in the "img" folder
-img_name = "img/"+your_description.replace(" ", "_")+".jpg"
-
-img.save(img_name)
+while True:
+    your_description = input("Enter your description for the image you want: ")
+    # print(your_description)
+    response = openai.Image.create(
+        prompt=your_description,
+        n=1,
+        size="1024x1024"
+    )
+    # Get the URL of the generated image
+    image_url = response['data'][0]['url']
+    # Use requests to retrieve the image content from the URL
+    response = requests.get(image_url)
+    # Use the Image package from PIL to open the image content
+    img = Image.open(io.BytesIO(response.content))
+    # Save the image to a file named "yune.jpg" in the "img" folder
+    img_name = "img/"+your_description.replace(" ", "_")+".jpg"
+    
+    img.save(img_name)
+    
--- text_completion.py
+++ text_completion.py
@@ -4,19 +4,21 @@ import os
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Define the prompt
-prompt = "How does a message queueing system achieve message integrity; validity? How does it make sure that message is delivered at least once."
-
-your_description = input("Enter your question for GPT-3 model: ")
-# Generate the completion
-completion = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=your_description,
-    max_tokens=1000,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
-
-# Get the response
-message = completion.choices[0].text
-print(message)
+while True:
+    prompt = "How does a message queueing system achieve message integrity; validity? How does it make sure that message is delivered at least once."
+    
+    your_description = input("Enter your question for GPT-3 model: ")
+    # Generate the completion
+    completion = openai.Completion.create(
+        engine="davinci-instruct-beta",
+        prompt=your_description,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    # Get the response
+    message = completion.choices[0].text
+    print(message)
+    
