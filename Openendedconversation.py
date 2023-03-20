041751df5ca0a42e56cf7119c22e7cbe6ce1e5a0
Zheng Yune <yunethecoder@gmail.com>
2023-03-20 08:21:10 -0500

    update 3.5

--- Openendedconversation.py
+++ Openendedconversation.py
@@ -5,18 +5,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def chat_with_gpt3(prompt):
-    completions = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
+    completions = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": prompt}
+    ]
+)
 
-        n=1,
-        stop=None,
-        temperature=0.5,
-        max_tokens=1000,
-        top_p=1,
-    )
-
-    message = completions.choices[0].text
+    message = completions['choices'][0]['message']['content']
     return message
 
 
